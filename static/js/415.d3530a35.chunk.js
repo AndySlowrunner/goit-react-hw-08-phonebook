30c2b558053a2d07fe0d76399c80bb5f55dfe79d
AndySlowrunner <112825482+AndySlowrunner@users.noreply.github.com>
2023-11-25 06:47:40 +0000

    Deploying to gh-pages from @ AndySlowrunner/goit-react-hw-08-phonebook@e939b0dee97127ac59f747897921c54118ad5130 🚀

---
+++ static/js/415.d3530a35.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:(e,r,t)=>{t.r(r),t.d(r,{default:()=>n});var a=t(184);function n(){return(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["Create your Contact "," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.d3530a35.chunk.js.map
